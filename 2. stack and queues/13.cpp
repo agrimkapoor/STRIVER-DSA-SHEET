647a5be11ca6d9ce786f1cff184270ca597a1ff0
agrimkapoor <agrimkapoor07@gmail.com>
2026-08-11 11:01:14 +0530

    Update 13.cpp

--- 2. stack and queues/13.cpp
+++ 2. stack and queues/13.cpp
@@ -1,49 +1,53 @@
-// LRU cache
+//LRU cache
 
 class LRUCache {
 public:
-    list<pair<int,int>>l;//key value pair
+    list<pair<int,int>>l;//key value
     unordered_map<int,list<pair<int,int>>::iterator>mp;
     int cap;
     LRUCache(int capacity) {
-        cap = capacity;
+     cap = capacity;   
     }
     
     int get(int key) {
-        if(mp.find(key)==mp.end())return -1;
+        if(mp.find(key) == mp.end())return -1;
 
-        auto it = mp[key];
+        auto it = mp[key];//address of node
         int value = it->second;
-       
+
         l.erase(it);
+
         l.push_front({key,value});
         mp[key] = l.begin();
+
         return value;
+
     }
     
     void put(int key, int value) {
-
-        if(mp.find(key)!=mp.end()){
-            auto it = mp[key];
-            l.erase(it);
-            
-            
-            l.push_front({key,value});
-            mp[key] = l.begin();
-        }
-        else{
-            if(cap==0){
+      if(mp.find(key)==mp.end()){
+            if(cap == 0){
                 auto it = prev(l.end());
-                mp.erase(it->first);
+                int KEY = it->first;
                 l.erase(it);
+                mp.erase(KEY);
+
+                l.push_front({key,value});
+                mp[key]=l.begin();
             }
             else{
+                l.push_front({key,value});
+                mp[key]=l.begin();
                 cap--;
             }
-            l.push_front({key,value});
-            mp[key] = l.begin();
-        }
-        
+      } 
+      else{
+        auto it = mp[key];
+        l.erase(it);
+
+        l.push_front({key,value});
+        mp[key] = l.begin();
+      } 
     }
 };
 
